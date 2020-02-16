a5748d30a4f09612e981a056dbe72cf94ac3f429
chantelletjie <chantellelandman710@gmail.com>
2020-02-16 20:39:04 +0200

    Updates

---
+++ precache-manifest.b67ee21b52372c8d160f4f929cc3f068.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c1781080f393f504d68d5e5497082e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b2501a153361e7106eb0",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "30ed121ed4a5b15a20ad",
+    "url": "/robofriends/static/css/main.8231be42.chunk.css"
+  },
+  {
+    "revision": "b2501a153361e7106eb0",
+    "url": "/robofriends/static/js/2.e4e98ab1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robofriends/static/js/2.e4e98ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30ed121ed4a5b15a20ad",
+    "url": "/robofriends/static/js/main.2b33239f.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
